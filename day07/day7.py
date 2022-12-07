2dbd2b40cdcd7d858cc3c8d57158ee7fd618964a
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2022-12-07 06:51:33 +0100

    Add Day 7

--- day07/day7.py
+++ day07/day7.py
@@ -1,30 +1,34 @@
 def part1():
-    pass
+    system = read_input()
+    print(sum([size[0] for size in system.values() if size[0] <= 100000]))
 
 
 def part2():
-    pass
+    system = read_input()
+    print(min([size[0] for size in system.values() if system['/'][0] - size[0] <= 40000000]))
 
 
 def read_input():
-    path = [{}]
-    cursor = path[-1]
-    system = {'/': cursor}
+    path = [[0]]
+    absolute_path = '/'
+    system = {absolute_path: path[-1]}
     for line in open('day7.txt').read().split('\n')[1:]:
         if line.startswith('$ ls'):
             pass
         elif line.startswith('dir'):
-            cursor[line.split('dir ')[1]] = {}
+            system[absolute_path + '/' + line.split('dir ')[1]] = [0]
         elif line[0].isnumeric():
             size, name = tuple(line.split())
-            cursor[name] = int(size)
+            size = int(size)
+            for n in path:
+                n[0] += size
         elif line.startswith('$ cd ..'):
             path.pop()
-            cursor = path[-1]
+            absolute_path = absolute_path[:len(absolute_path) - absolute_path[::-1].index('/') - 1]
         elif line.startswith('$ cd'):
             name = line.split('$ cd ')[1]
-            cursor = cursor[name]
-            path.append(cursor)
+            absolute_path += '/' + name
+            path.append(system[absolute_path])
     return system
 
 
