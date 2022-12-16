6c028ebac85a1d1303716e104dbf4d04327cebe1
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2022-12-16 10:46:53 +0100

    Add Day 16 Part 2

--- days/day16.py
+++ days/day16.py
@@ -40,7 +40,41 @@ class Day16(Day):
         return result
 
     def part2(self):
-        pass
+        grid = self.parse_input()
+        edges = self.shortest_paths()
+
+        best_scores = dict()
+        best_path = float('-inf')
+        paths = [('AA', 'AA', 0, 0, 0, frozenset())]
+
+        while len(paths) > 0:
+            cursor_a, cursor_b, minutes_a, minutes_b, total_flow, visited = paths.pop()
+            best_path = max(best_path, total_flow)
+            for (target, distance) in edges[cursor_a].items():
+                next_targets = visited.union({target})
+                next_total_flow = total_flow + grid[target][0] * (26 - minutes_a - distance)
+
+                if best_scores.get(next_targets, float('-inf')) >= next_total_flow:
+                    continue
+
+                if target not in visited and distance + minutes_a < 26:
+                    best_scores[next_targets] = next_total_flow
+                    paths.append((target, cursor_b, distance + minutes_a, minutes_b,
+                                  next_total_flow, visited.union({target})))
+
+            for (target, distance) in edges[cursor_b].items():
+                next_targets = visited.union({target})
+                next_total_flow = total_flow + grid[target][0] * (26 - minutes_b - distance)
+
+                if best_scores.get(next_targets, float('-inf')) >= next_total_flow:
+                    continue
+
+                if target not in visited and distance + minutes_b < 26:
+                    best_scores[next_targets] = next_total_flow
+                    paths.append((cursor_a, target, minutes_a, distance + minutes_b,
+                                  next_total_flow, visited.union({target})))
+        return best_path
+
 
     def parse_input(self):
         values = dict()
