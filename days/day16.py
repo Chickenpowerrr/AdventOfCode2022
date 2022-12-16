16c9322f640af3ea5eb34a69d5e1ce8383753ff4
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2022-12-16 08:51:18 +0100

    Add Day 16 Part 1

--- days/day16.py
+++ days/day16.py
@@ -6,27 +6,38 @@ from lib.day import Day
 class Day16(Day):
     def part1(self):
         grid = self.parse_input()
-        base_paths = {name: (0, set()) for name, (flow_rate, _) in grid.items()}
-        optimal_paths = {-1: base_paths, -2: base_paths}
+        edges = self.shortest_paths()
 
-        for minutes_left in range(30):
-            next_optimal_paths = {}
-            optimal_paths[minutes_left] = next_optimal_paths
+        best_scores = dict()
+        best_path = float('-inf')
+        paths = [('AA', 0, 0, frozenset())]
 
-            for name, (total_flow, opened) in optimal_paths[minutes_left - 1].items():
-                for other_name in grid[name][1]:
-                    moving_flow = total_flow
-                    opening_flow = optimal_paths[minutes_left - 2][other_name][0] \
-                                   + minutes_left * grid[name][0] \
-                        if other_name in optimal_paths[minutes_left - 2] else -1
-                    if name not in opened and opening_flow > next_optimal_paths\
-                            .get(name, (-1, ))[0]:
-                        next_optimal_paths[name] = opening_flow, opened.union({name})
-                    if moving_flow > next_optimal_paths.get(other_name, (-1, ))[0]:
-                        next_optimal_paths[other_name] = moving_flow, opened
+        while len(paths) > 0:
+            cursor, minutes, total_flow, visited = paths.pop()
+            best_path = max(best_path, total_flow)
+            for (target, distance) in edges[cursor].items():
+                next_targets = visited.union({target})
+                next_total_flow = total_flow + grid[target][0] * (30 - minutes - distance)
 
-        # BB, CC, DD, EE, HH, and JJ
-        return optimal_paths[29]
+                if best_scores.get(next_targets, float('-inf')) >= next_total_flow:
+                    continue
+
+                if target not in visited and distance + minutes < 30:
+                    best_scores[next_targets] = next_total_flow
+                    paths.append((target, distance + minutes,
+                                  next_total_flow, visited.union({target})))
+        return best_path
+
+    def shortest_paths(self):
+        grid = self.parse_input()
+        result = {key: ({key: 1} if grid[key][0] > 0 else {}) for key in grid.keys()}
+        for _ in range(len(grid)):
+            for name, (_, next_names) in grid.items():
+                for next_name in next_names:
+                    for trans_name, trans_price in result[next_name].items():
+                        result[name][trans_name] = \
+                            min(result[name].get(trans_name, float('inf')), trans_price + 1)
+        return result
 
     def part2(self):
         pass
@@ -35,11 +46,5 @@ class Day16(Day):
         values = dict()
         for line in self.read_input().split('\n'):
             pattern = re.search('Valve (.+) has flow rate=(\\d+); tunnels? leads? to valves? (.+)', line)
-            source = pattern.group(1)
-            values[source] = (int(pattern.group(2)), values.get(source, (-1, set()))[1])
-            for target in pattern.group(3).split(', '):
-                flow, predecessors = values.get(target, (-1, set()))
-                predecessors.add(source)
-                values[target] = (flow, predecessors)
-        print(values)
+            values[pattern.group(1)] = (int(pattern.group(2)), pattern.group(3).split(', '))
         return values
