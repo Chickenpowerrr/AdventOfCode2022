50d9fed8c2db4c9d9c15822d21b906f3d0257dd3
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2022-12-21 08:05:27 +0100

    Add Day 21 part 2

--- days/day21.py
+++ days/day21.py
@@ -5,7 +5,42 @@ from lib.day import Day
 
 class Day21(Day):
     def part1(self):
-        dependents, known = self.parse_input()
+        dependents, known, _ = self.parse_input()
+        return self.simulate(known, dependents)['root']
+
+    def part2(self):
+        dependents, start_known, (root_a, root_b) = self.parse_input()
+
+        known = start_known.copy()
+        known['humn'] = 0
+        result = self.simulate(known, dependents)
+        result = result[root_a], result[root_b]
+
+        decreasing = result[0] > result[1]
+
+        left_index = 0
+        right_index = 100000000000000
+
+        while True:
+            if left_index > right_index:
+                return None
+
+            middle_index = (right_index + left_index) // 2
+            known = start_known.copy()
+            known['humn'] = middle_index
+            result = self.simulate(known, dependents)
+            result = result[root_a], result[root_b]
+
+            if result[0] == result[1]:
+                return middle_index
+
+            if result[0] > result[1] and decreasing:
+                left_index = middle_index + 1
+            else:
+                right_index = middle_index - 1
+
+
+    def simulate(self, known, dependents):
         new_known = set(known.keys())
 
         while 'root' not in known:
@@ -19,16 +54,15 @@ class Day21(Day):
                 if a not in known or b not in known:
                     continue
 
-                known[target] = operator(a, b)
+                known[target] = operator(known[a], known[b])
                 new_known.add(target)
-        return known['root']
+        return known
 
-    def part2(self):
-        pass
 
     def parse_input(self):
         dependent = dict()
         known = dict()
+        roots = None
         for line in self.read_input().split('\n'):
             if line[6].isdigit():
                 name, value = line.split(': ')
@@ -37,15 +71,22 @@ class Day21(Day):
 
             name, a, operator, b = re.search('(.+?): (.+?) ([+\\-*/]) (.+)', line).groups()
             if operator == '+':
-                operator = lambda x, y: known[x] + known[y]
+                operator = lambda x, y: x + y if type(x) == type(y) == int \
+                    else f'({x} + {y})'
             elif operator == '-':
-                operator = lambda x, y: known[x] - known[y]
+                operator = lambda x, y: x - y if type(x) == type(y) == int \
+                    else f'({x} - {y})'
             elif operator == '*':
-                operator = lambda x, y: known[x] * known[y]
+                operator = lambda x, y: x * y if type(x) == type(y) == int \
+                    else f'({x} * {y})'
             elif operator == '/':
-                operator = lambda x, y: known[x] // known[y]
+                operator = lambda x, y: x // y if type(x) == type(y) == int \
+                    else f'({x} / {y})'
 
             dependent[a] = (name, a, b, operator)
             dependent[b] = (name, a, b, operator)
 
-        return dependent, known
+            if name == 'root':
+                roots = a, b
+
+        return dependent, known, roots
